f42a2e27abce4ae27e145f80320a7bf48338ce29
Pramit Govindaraj <pramit.govindaraj@gmail.com>
2026-04-11 13:07:29 -0400

    Added untested CUDA code

--- conway.cu
+++ conway.cu
@@ -1,16 +1,19 @@
-#include<stdio.h>
-#include<cuda.h>
-#include<cuda_runtime.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+#define BLOCK_SIZE (16)
 
 extern "C" 
 {
 int setCudaDevice(int rank);
-void runCuda(int device, int *x, int *y, size_t w, size_t h);
-void cudaUnifiedMalloc(int** x, size_t nBytes);
+void runCuda(int device, bool *x, bool *y, size_t w, size_t h);
+void cudaUnifiedMalloc(bool** x, size_t nBytes);
 void cudaUnifiedFree(void* x);
 }
 
-__global__ void Conway(int *x, int *y, size_t w, size_t h);
+__global__ void Conway(bool *x, bool *y, size_t w, size_t h);
 
 int setCudaDevice(int rank) {
   int numCudaDevices;
@@ -20,12 +23,17 @@ int setCudaDevice(int rank) {
   return device;
 }
 
-void runCuda(int device, int *x, int *y, size_t w, size_t h)
+void runCuda(int device, bool *x, bool *y, size_t w, size_t h)
 {
-  cudaMemPrefetchAsync(x - haloSz, ((2 * haloSz) + n) * sizeof(int), device, 0);
-  cudaMemPrefetchAsync(y, n * sizeof(int), device, 0);
-  size_t sharedMemSize = (1024 + 2 * haloSz) * sizeof(int);
-  Conway<<<16384,1024, sharedMemSize>>>(x, y, w, h);
+  cudaMemPrefetchAsync(x, (w + 2) * (h + 2) * sizeof(bool), device, 0);
+  cudaMemPrefetchAsync(y, (w + 2) * (h + 2) * sizeof(bool), device, 0);
+
+  int threadsPerBlock = (BLOCK_SIZE * BLOCK_SIZE);
+  int gridX = (w + BLOCK_SIZE - 1) / BLOCK_SIZE;
+  int gridY = (h + BLOCK_SIZE - 1) / BLOCK_SIZE;
+  int totalBlocks = gridX * gridY;
+
+  Conway<<<totalBlocks, threadsPerBlock>>>(x, y, w, h);
   cudaDeviceSynchronize();
 }
 
@@ -38,43 +46,71 @@ void cudaUnifiedFree(void* x) {
 }
 
 __global__
-void Conway(int *x, int *y, size_t w, size_t h)
+void Conway(bool *in, bool *out, size_t w, size_t h)
 {
-  extern __shared__ int s_data[];
+  __shared__ bool tile[BLOCK_SIZE + 2][BLOCK_SIZE + 2];
 
-  int stride = blockDim.x * gridDim.x;
+  int tx = threadIdx.x % BLOCK_SIZE;
+  int ty = threadIdx.x / BLOCK_SIZE;
 
-  for (int base = blockIdx.x * blockDim.x; base < n; base += stride) {
-    int global_idx = base + threadIdx.x;
-    if(global_idx < n + haloSz) {
-      s_data[haloSz + threadIdx.x] = x[global_idx];
-    } else {
-      s_data[haloSz + threadIdx.x] = 0;
-    }
+  int blocksPerRow = (w + BLOCK_SIZE - 1) / BLOCK_SIZE;
+  int bx = blockIdx.x % blocksPerRow;
+  int by = blockIdx.x / blocksPerRow;
 
-    for (int h = threadIdx.x; h < haloSz; h += blockDim.x) {
-      s_data[h] = x[base + h - haloSz]; 
-    }
+  int gx = (bx * BLOCK_SIZE) + tx + 1;
+  int gy = (by * BLOCK_SIZE) + ty + 1;
 
-    for (int h = threadIdx.x; h < haloSz; h += blockDim.x) {
-        int right_idx = base + blockDim.x + h;
-        if(right_idx < n + haloSz) {
-          s_data[haloSz + blockDim.x + h] = x[right_idx];
-        } else {
-          s_data[haloSz + blockDim.x + h] = 0;
-        }
-    }
+  int stride = w + 2;
 
-    __syncthreads();
+  bool right = (tx == BLOCK_SIZE - 1 || gx == w);
+  bool bottom = (ty == BLOCK_SIZE - 1 || gy == h);
 
-    if (global_idx < n) {
-        int result = 0;
-        for (int j = 0; j <= 2 * haloSz; ++j) {
-            result += s_data[threadIdx.x + j];
-        }
-        y[global_idx] = result;
-    }
-
-    __syncthreads(); 
+  if (gx <= w && gy <= h) {
+    tile[ty + 1][tx + 1] = in[gy * stride + gx];
+  } else {
+    tile[ty + 1][tx + 1] = 0;
   }
+
+  if (tx == 0 && gy <= h) {
+    tile[ty + 1][0] = in[gy * stride + (gx - 1)];
+  }
+  if (right && gy <= h && gx <= w) {
+    tile[ty + 1][tx + 2] = in[gy * stride + (gx + 1)];
+  }
+  if (ty == 0 && gx <= w) {
+    tile[0][tx + 1] = in[(gy - 1) * stride + gx];
+  }
+  if (bottom && gx <= w && gy <= h) {
+    tile[ty + 2][tx + 1] = in[(gy + 1) * stride + gx];
+  }
+
+  if (tx == 0 && ty == 0) { 
+    tile[0][0] = in[(gy - 1) * stride + (gx - 1)];
+  }
+  if (right && ty == 0 && gx <= w) {
+    tile[0][tx + 2] = in[(gy - 1) * stride + (gx + 1)];
+  }
+  if (tx == 0 && bottom && gy <= h) {
+    tile[ty + 2][0] = in[(gy + 1) * stride + (gx - 1)];
+  }
+  if (right && bottom && gx <= w && gy <= h) {
+    tile[ty + 2][tx + 2] = in[(gy + 1) * stride + (gx + 1)];
+  }
+
+  __syncthreads();
+
+  if (gx <= w && gy <= h) {
+    int aliveNeighbors = tile[ty][tx] + 
+      tile[ty][tx + 1] + 
+      tile[ty][tx + 2] +
+      tile[ty + 1][tx] + 
+      tile[ty + 1][tx + 2] +
+      tile[ty + 2][tx] + 
+      tile[ty + 2][tx + 1] + 
+      tile[ty + 2][tx + 2];
+
+    bool current_state = tile[ty + 1][tx + 1];
+    
+    out[gy * stride + gx] = (current_state && (aliveNeighbors == 2 || aliveNeighbors == 3)) || (!current_state && aliveNeighbors == 3);
+  }  
 }
